531d5828544bbf2d7286774bf20db7cb32bd147b
Anirudh Kumar Subramanyam <ask710@uw.edu>
2018-04-18 13:28:55 -0700

    finish context and session state, start auth:

--- servers/gateway/handlers/auth.go
+++ servers/gateway/handlers/auth.go
@@ -1,6 +1,38 @@
 package handlers
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/info344-s18/challenges-ask710/servers/gateway/models/users"
+)
+
 //TODO: define HTTP handler functions as described in the
 //assignment description. Remember to use your handler context
 //struct as the receiver on these functions so that you have
 //access to things like the session store and user store.
+
+//UserHandler handles requests for the "users" resource
+func (ctx *Context) UserHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		if !strings.HasPrefix(r.Header.Get(headerContentType), contentTypeJSON) {
+			http.Error(w, "Invalid media type", http.StatusUnsupportedMediaType)
+			return
+		}
+
+		newUser := &users.NewUser{}
+		if err := json.NewDecoder(r.Body).Decode(newUser); err != nil {
+			http.Error(w, fmt.Sprintf("error decoding JSON: %v", err), http.StatusBadRequest)
+			return
+		}
+		user, err := newUser.ToUser()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Invalid user: %v", err), http.StatusInternalServerError)
+		}
+		ctx.UserStore.Insert(user)
+
+	}
+}
--- servers/gateway/handlers/context.go
+++ servers/gateway/handlers/context.go
@@ -1,8 +1,30 @@
 package handlers
 
+import (
+	"github.com/info344-s18/challenges-ask710/servers/gateway/models/users"
+	"github.com/info344-s18/challenges-ask710/servers/gateway/sessions"
+)
+
 //TODO: define a handler context struct that
 //will be a receiver on any of your HTTP
 //handler functions that need access to
 //globals, such as the key used for signing
 //and verifying SessionIDs, the session store
 //and the user store
+
+//Context is a handler context struct that
+//will be a receiver for handler functions
+type Context struct {
+	SigningKey   string
+	SessionStore sessions.Store
+	UserStore    users.Store
+}
+
+//NewContext constructs a new Context
+func NewContext(signingKey string, sessionStore sessions.Store, userStore users.Store) *Context {
+	return &Context{
+		SigningKey:   signingKey,
+		SessionStore: sessionStore,
+		UserStore:    userStore,
+	}
+}
