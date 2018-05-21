d13fdc905322ca875f1c814e2b579a37f62f6909
Anirudh Kumar Subramanyam <ask710@uw.edu>
2018-05-20 22:46:03 -0700

    start notifier and websockets

---
+++ servers/gateway/handlers/notifier.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+//Notifier handles WebSocket Notifications
+type Notifier struct {
+	eventQ          chan []byte
+	currConnections map[int64]*websocket.Conn
+	mx              sync.Mutex
+}
+
+//NewNotifier constructs a new Notifier
+func NewNotifier() *Notifier {
+	n := &Notifier{
+		eventQ:          make(chan []byte, 1024),
+		currConnections: make(map[int64]*websocket.Conn),
+	}
+
+	return n
+}
+
+//AddClient adds a new client to the Notifier
+func (n *Notifier) AddClient(client *websocket.Conn, userID int64) {
+	n.mx.Lock()
+	defer n.mx.Unlock()
+	n.currConnections[userID] = client
+	go n.readLoop(client, userID)
+}
+
+func (n *Notifier) readLoop(client *websocket.Conn, userID int64) {
+	for {
+		if _, _, err := client.NextReader(); err != nil {
+			client.Close()
+			delete(n.currConnections, userID)
+			break
+		}
+	}
+}
--- servers/gateway/handlers/websockets.go
+++ servers/gateway/handlers/websockets.go
@@ -1,10 +1,55 @@
 package handlers
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/info344-s18/challenges-ask710/servers/gateway/sessions"
+)
+
 //TODO: add a handler that upgrades clients to a WebSocket connection
 //and adds that to a list of WebSockets to notify when events are
 //read from the RabbitMQ server. Remember to synchronize changes
 //to this list, as handlers are called concurrently from multiple
 //goroutines.
+
+//WebSocket is a struct for upgrade requests
+type WebSocket struct {
+	notifier *Notifier
+	upgrader *websocket.Upgrader
+}
+
+//NewWebSocketsHandler constructs a new WebSocketsHandler
+func NewWebSocket(notifier *Notifier) *WebSocket {
+	//create, initialize, and return a new WebSocketsHandler
+	return &WebSocket{
+		notifier: notifier,
+		upgrader: &websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+			CheckOrigin:     func(r *http.Request) bool { return true },
+		},
+	}
+}
+
+//WebSocketHandler upgrades connection to a WebSocket
+//TODO: add websocket to context and upgrade connection
+//TODO: add the handler to gateway under resource path /v1/ws
+func (ctx *Context) WebSocketHandler(w http.ResponseWriter, r *http.Request) {
+	stateStruct := &SessionState{}
+	_, err := sessions.GetState(r, ctx.SigningKey, ctx.SessionStore, stateStruct)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error getting session state: %v", err), http.StatusUnauthorized)
+		return
+	}
+	//add websocket to context
+	// conn, err := wsh.upgrader.Upgrade(w, r, nil)
+	// if err != nil {
+	// 	return
+	// }
+
+}
 
 //TODO: start a goroutine that connects to the RabbitMQ server,
 //reads events off the queue, and broadcasts them to all of
